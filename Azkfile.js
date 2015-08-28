c3157ee4097351680d164ab7a69988edafa9dedd
Julio Makdisse Saito <saitodisse@gmail.com>
2015-08-28 18:03:01 -0300

    [azk] Azkfile.md and deploy sys

--- Azkfile.js
+++ Azkfile.js
@@ -34,7 +34,10 @@ systems({
     },
     scalable: {"default": 1},
     http: {
-      domains: [ "#{system.name}.#{azk.default_domain}" ]
+      domains: [
+        "#{system.name}.#{azk.default_domain}", // default azk
+        '#{process.env.AZK_HOST_IP}'            // used if deployed
+      ]
     },
     ports: {
       // exports global variables
@@ -86,5 +89,43 @@ systems({
 
       RACK_ENV: "production",
     },
+  },
+
+  deploy: {
+    image: {"docker": "azukiapp/deploy-digitalocean"},
+    mounts: {
+
+      // your files on remote machine
+      // will be on /home/git folder
+      "/azk/deploy/src":  path("."),
+
+      // will use your public key on server
+      // that way you can connect with:
+      // $ ssh git@REMOTE.IP
+      // $ bash
+      "/azk/deploy/.ssh": path("#{process.env.HOME}/.ssh")
+    },
+
+    // this is not a server
+    // just call with azk shell deploy
+    scalable: {"default": 0, "limit": 0},
+
+    envs: {
+      GIT_CHECKOUT_COMMIT_BRANCH_TAG: 'azkfile',
+      AZK_RESTART_COMMAND: 'azk restart -Rvv',
+      RUN_SETUP: 'true',
+      RUN_CONFIGURE: 'true',
+      RUN_DEPLOY: 'true',
+    }
+  },
+  "fast-deploy": {
+    extends: 'deploy',
+    envs: {
+      GIT_CHECKOUT_COMMIT_BRANCH_TAG: 'azkfile',
+      AZK_RESTART_COMMAND: 'azk restart -Rvv',
+      RUN_SETUP: 'false',
+      RUN_CONFIGURE: 'false',
+      RUN_DEPLOY: 'true',
+    }
   },
 });
